bb4a0fcd131a4a53fb0e904026b2b20c75acf77d
Mariyalintu <116860342+Mariyalintu@users.noreply.github.com>
2023-06-23 14:14:45 +0530

    Add files via upload

---
+++ package1.java
@@ -0,0 +1,13 @@
+import graphics.*;
+class Main
+{
+public static void main(String args[])
+{
+graphics.Areas r1=new graphics.Areas();
+r1.Rect();
+r1.Triangle();
+r1.square();
+r1.circle();
+}
+}
+
